8accc2fb283df5f3a2a61e25d22bc4c1bd0b51be
Soonas <49923278+Soonas@users.noreply.github.com>
2020-03-24 21:53:17 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+
+//LISTAGEM DE ONGS
+module.exports = {
+    async index (request, response){
+        const ongs = await connection('ongs').select('*');
+    
+        return response.json(ongs);
+    },
+//CADASTRO
+    async create(request, response) {
+        const {name, email, whatsapp, city, uf} = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+    
+        return response.json({id});
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express'); //importando funcionalidades do express
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express(); //variavel que armazena a aplicação(app) >> [mais tarde essa app vai possuir as rotas]
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333); //redireciona o app para a porta que eu quero
+
+/*  Rota / Recurso
+Métodos HTTP:
+GET:  Buscar uma informação do back-end
+POST: Criar uma informação no back-end
+PUT: Alterar uma informação no back-end
+DELETE: Deletar uma informação no back-end
+ */
+
+ /*Tipos de Parametros
+ Query: Parâmetros nomeados e enviados na rota após "?" (Filtros, páginação...)
+ Route Params: Parâmetros utilizados para identificar recursos
+ Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+ */
+
+ /* 
+ SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server...
+ NoSQL: MongoDB, CouchDB...(não relacionanais)
+ */
+
+ /* 
+Driver: SELECT * FROM users
+Queery Builder: table('users').select('*')where()
+ */
+
